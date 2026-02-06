69a6e358464e101c41ddbf5a5f6518bf6e1da6e6
michal92299 <chudybyczek2137@gmail.com>
2026-02-06 10:28:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,20 @@
+# 1. Tworzenie katalogu (użycie zmiennej $USER)
+mkdir -p /home/$USER/.hackeros/h-sharp
 
+# 2. Pobieranie pliku do /tmp
+wget https://github.com/HackerOS-Linux-System/H-Sharp/releases/download/v0.2.0/bin.zip -P /tmp
+
+# 3. Rozpakowanie zawartości do tymczasowego folderu
+unzip /tmp/bin.zip -d /tmp/hsharp_temp
+
+# 4. Nadanie uprawnień wykonywania
+chmod a+x /tmp/hsharp_temp/*
+
+# 5. Przenoszenie binarek (użycie sudo dla /usr/bin/)
+sudo mv /tmp/hsharp_temp/virus-go /usr/bin/
+sudo mv /tmp/hsharp_temp/virus /usr/bin/
+mv /tmp/hsharp_temp/hst /home/$USER/.hackeros/h-sharp/
+mv /tmp/hsharp_temp/hs-errors /home/$USER/.hackeros/h-sharp/
+
+# 6. Porządkowanie katalogu /tmp
+rm -rf /tmp/hsharp_temp /tmp/bin.zip
