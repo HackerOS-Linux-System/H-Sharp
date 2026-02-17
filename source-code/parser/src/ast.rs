e22f28e25c0f71b3ee7d9d12b60225468eff6fa5
michal92299 <chudybyczek2137@gmail.com>
2026-02-17 19:05:43 +0100

    Update ast.rs

--- source-code/parser/src/ast.rs
+++ source-code/parser/src/ast.rs
@@ -5,6 +5,8 @@ pub enum HSharpLiteral {
     Int(i32),
     Bool(bool),
     String(String),
+    RawString(String),
+    Byte(u8),
     Float(f64),
 }
 
@@ -13,6 +15,12 @@ pub enum HSharpOp {
     Add, Sub, Mul, Div, Mod,
     Eq, Ne, Lt, Gt, Le, Ge,
     And, Or,
+    Shl, Shr, BitAnd, BitXor, BitOr,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone, Copy, PartialEq, Eq)]
+pub enum UnaryOp {
+    Neg, BitNot, Deref, AddrOf
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq)]
@@ -23,15 +31,21 @@ pub enum HType {
     Bool,
     F32,
     F64,
+    U8,
+    U16,
+    U32,
+    U64,
     Pointer(Box<HType>),
     Array(Box<HType>, usize),
     Slice(Box<HType>),
-    // Struct supports generic arguments: e.g. List<Int>
     Struct(String, Vec<HType>),
-    // Generic placeholder: T, U, V inside a struct definition
     Generic(String),
     Union(String),
     Unit,
+    Tuple(Vec<HType>),
+    Enum(String),
+    Result(Box<HType>, Box<HType>),
+    Option(Box<HType>),
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
@@ -40,13 +54,12 @@ pub enum HSharpExpr {
     Var(String),
     Alloc(Box<HSharpExpr>),
     Dealloc(Box<HSharpExpr>),
-    Deref(Box<HSharpExpr>),
+    Unary(UnaryOp, Box<HSharpExpr>),
     Assign(Box<HSharpExpr>, Box<HSharpExpr>),
     Write(Box<HSharpExpr>),
     Block(Vec<HSharpStmt>),
     Direct(Box<HSharpExpr>),
     BinOp(HSharpOp, Box<HSharpExpr>, Box<HSharpExpr>),
-    AddrOf(Box<HSharpExpr>),
     If(Box<HSharpExpr>, Box<HSharpExpr>, Option<Box<HSharpExpr>>),
     While(Box<HSharpExpr>, Box<HSharpExpr>),
     For(String, Box<HSharpExpr>, Box<HSharpExpr>, Box<HSharpExpr>),
@@ -58,23 +71,39 @@ pub enum HSharpExpr {
     Cast(HType, Box<HSharpExpr>),
     SizeOf(HType),
     StructLit(String, Vec<HSharpExpr>),
-    UnionLit(String, String, Box<HSharpExpr>),
+    // Enum/Union literal: EnumName, VariantName, Args
+    EnumLit(String, String, Vec<HSharpExpr>),
     ArrayLit(Vec<HSharpExpr>),
     Field(Box<HSharpExpr>, String),
     Index(Box<HSharpExpr>, Box<HSharpExpr>),
     Return(Box<HSharpExpr>),
+    Tuple(Vec<HSharpExpr>),
+    Closure(Vec<String>, Box<HSharpExpr>),
+    Await(Box<HSharpExpr>),
+    OptionalChain(Box<HSharpExpr>),
+    Range(Box<HSharpExpr>, Box<HSharpExpr>, bool),
+    Try(Box<HSharpExpr>),
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub enum EnumVariant {
+    Unit,
+    Tuple(Vec<HType>),
+    Struct(Vec<(String, HType)>),
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub enum HSharpStmt {
     Let(String, Option<HType>, HSharpExpr),
     Expr(HSharpExpr),
-    FunctionDef(String, Vec<(String, HType)>, HType, Option<Box<HSharpExpr>>),
-    // StructDef supports generic parameters: struct List<T> { ... }
+    FunctionDef(String, Vec<(String, HType)>, HType, Option<Box<HSharpExpr>>, bool), // Added is_async bool
     StructDef(String, Vec<String>, Vec<(String, HType)>),
     UnionDef(String, Vec<(String, HType)>),
     Impl(String, Vec<HSharpStmt>),
     Import(String, Vec<RequireItem>),
+    ConstDef(String, HType, HSharpExpr),
+    TypeAlias(String, HType),
+    EnumDef(String, Vec<(String, EnumVariant)>), // Variant name + Variant Type
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
