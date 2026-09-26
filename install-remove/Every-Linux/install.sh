dada91542982b0e74dbb469bea6cba9c5b3a0663
michal92299 <chudybyczek2137@gmail.com>
2026-09-26 09:16:16 +0200

    Update install.sh

--- install-remove/Every-Linux/install.sh
+++ install-remove/Every-Linux/install.sh
@@ -1,24 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-# Sprawdzenie, czy skrypt jest uruchomiony jako root
-if [ "$EUID" -ne 0 ]; then
-  echo "Błąd: Uruchom ten skrypt za pomocą sudo!"
-  exit 1
-fi
+# Pobranie głównego narzędzia H#
+sudo curl -L https://github.com/HackerOS-Linux-System/H-Sharp/releases/download/v0.9/hsharp -o /usr/bin/h#
+sudo chmod a+x /usr/bin/h#
 
-echo "Rozpoczynam pobieranie składników języka H#..."
+# Utworzenie katalogu dla bibliotek standardowych
+sudo mkdir -p /usr/lib/HackerOS
+sudo mkdir -p /usr/lib/HackerOS/H#
 
-# 1. Pobieranie pliku 'bytes' do /usr/bin/bytes
-echo "Pobieranie: bytes -> /usr/bin/bytes"
-curl -L "https://github.com/HackerOS-Linux-System/H-Sharp/releases/download/v0.7/bytes" -o /usr/bin/bytes
-
-# 2. Pobieranie binarki 'hsharp' i zapisanie jej jako /usr/bin/h#
-echo "Pobieranie: hsharp -> /usr/bin/h#"
-curl -L "https://github.com/HackerOS-Linux-System/H-Sharp/releases/download/v0.7/hsharp" -o "/usr/bin/h#"
-
-# 3. Nadanie uprawnień wykonywalności dla obu plików
-echo "Nadawanie uprawnień wykonywalności..."
-chmod +x /usr/bin/bytes
-chmod +x "/usr/bin/h#"
-
-echo "Gotowe! Narzędzia H# zostały zainstalowane w /usr/bin."
+# Pobranie i wypakowanie katalogu std z repozytorium
+sudo curl -L https://github.com/HackerOS-Linux-System/H-Sharp/archive/refs/heads/main.tar.gz | tar -xz --strip-components=1 -C /usr/lib/HackerOS/H# H-Sharp-main/std
